e2834541356962598e6af6691aa7a33c140596e5
Valentyn Fediuk <valentyn@aff1.com>
2023-08-07 18:22:40 +0300

    [update] layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,19 @@ const {fullName} = staticText.author
 
   return (
     <main>
-        <div className='flex justify-center flex-col'>
-            <div className='mx-auto'>
+        <div className='flex justify-center flex-col py-4'>
+            <h1 className='text-center text-3xl animation-bounce'>Front-end Developer</h1>
+            <div className='mx-auto my-2'>
                 <Image
                     src="/avatar.jpeg"
                     alt="Avatar"
                     width={200}
                     height={200}
                     loading='eager'
-                    className='rounded-full'
+                    className='rounded-full hover:scale-110 transition-all'
                 />
             </div>
-            <h1 className='text-center'>{fullName}</h1>
+            <h2 className='text-center text-2xl'>{fullName}</h2>
         </div>
 
     </main>
